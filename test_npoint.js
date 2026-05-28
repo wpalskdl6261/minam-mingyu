8512f1a908006c9b236c9abf5c6ff26317d69f6d
wpalskdl6261 <wpalskdl6261@gmail.com>
2026-05-28 16:16:25 +0900

    Add files via upload

--- test_npoint.js
+++ test_npoint.js
@@ -1,12 +1,12 @@
 const https = require('https');
 
-https.get('https://api.npoint.io/244f85ea912d9e47ad92', (res) => {
+https.get('https://api.npoint.io/0fc011d95f8e6b2da1c3', (res) => {
     let data = '';
     res.on('data', chunk => data += chunk);
     res.on('end', () => console.log('Base:', res.statusCode, data));
 });
 
-https.get('https://api.npoint.io/244f85ea912d9e47ad92/10', (res) => {
+https.get('https://api.npoint.io/0fc011d95f8e6b2da1c3/10', (res) => {
     let data = '';
     res.on('data', chunk => data += chunk);
     res.on('end', () => console.log('Nested 10:', res.statusCode, data));
